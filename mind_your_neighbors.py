7232de573c190ecbafcf144a10a958972e8f3c32
François Schmidts <francois.schmidts@gmail.com>
2014-01-29 11:59:57 +0100

    introducing triggers

--- mind_your_neighbors.py
+++ mind_your_neighbors.py
@@ -13,6 +13,7 @@ config = ConfigParser(defaults={
         'loglevel': 'INFO',
         'error_on_stderr': 'true',
         'cache_file': '/run/mind_your_neighbors.cache',
+        'trigger': '3',
 })
 config.read(['/etc/mind_your_neighbors.conf',
     os.path.expanduser('~/.config/mind_your_neighbors.conf')])
@@ -32,9 +33,12 @@ def ip_neigh_match(regex, exclude=None):
         exclude = re.compile(exclude)
     for neighbor in stdout.splitlines():
         if exclude and exclude.match(neighbor):
+            logger.debug("line %r is excluded" % neighbor)
             continue
         if regex.match(neighbor):
+            logger.debug("line %r is a match" % neighbor)
             return True
+        logger.debug("line %r is no match" % neighbor)
     return False
 
 
@@ -44,20 +48,24 @@ def is_in_cache(key, value):
         return False
     with open(path, 'r') as fp:
         cache = json.load(fp)
-    logger.debug('got value %r from cache, comparing to %r'
-            % (cache.get(key), value))
-    return cache.get(key) == value
+    if cache.get(key, []).count(value) == config.getint(MAIN_SEC, 'trigger'):
+        return cache[key][-1] == value
+    return None
 
 
 def write_cache(key, value):
     path = config.get(MAIN_SEC, 'cache_file')
-    logger.info(path)
     if os.path.exists(path):
         with open(path, 'r') as fp:
             cache = json.load(fp)
     else:
         cache = {}
-    cache[key] = value
+    if key in cache:
+        cache[key].append(value)
+    else:
+        cache[key] = [value]
+    cache[key] = cache[key][-config.getint(MAIN_SEC, 'trigger'):]
+    logger.debug('write_cache: %r => %r' % (key, cache[key]))
     with open(config.get(MAIN_SEC, 'cache_file'), 'w') as fp:
         json.dump(cache, fp)
 
@@ -66,21 +74,29 @@ def main():
     for section in config.sections():
         if section == config.default_section:
             continue
+
         if ip_neigh_match(
                 config.get(section, 'regex'),
                 config.get(section, 'exclude', fallback=None)):
             cmd = config.get(section, 'command_match')
+            result = 'match'
         else:
             cmd = config.get(section, 'command_no_match')
-        if is_in_cache(section, cmd):
-            logger.debug('already in state : not launching %r' % cmd)
+            result = 'no_match'
+
+        in_cache = is_in_cache(section, result)
+        if in_cache is True:
             continue
-        logger.info('launching %r' % cmd)
+        if in_cache is None:
+            write_cache(section, result)
+            continue
+
+        logger.warn('launching %r' % cmd)
         process = Popen(cmd, shell=True, stdout=PIPE, stderr=PIPE)
-        results = process.communicate()
-        if config.getboolean(section, 'error_on_stderr') and results[1]:
-            logger.error(results[1])
-        write_cache(section, cmd)
+        stderr = process.communicate()[1]
+        if stderr and config.getboolean(section, 'error_on_stderr'):
+            logger.error(stderr)
+        write_cache(section, result)
 
 
 if __name__ == '__main__':
